57d7e4b09abad9224696a132ca2b06699b0bfef4
Sarthak Joshi <i82749@d2hs.com>
2018-04-11 16:56:24 +0545

    Get heroes with HttpClient

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { HeroService } from './hero.service';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
@@ -11,11 +12,14 @@ import { MessageService } from './message.service';
 import { AppRoutingModule } from './/app-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
+import { HttpInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
+
 /*
 the @NgModule decorator identifies AppModule as an NgModule class. 
 @NgModule takes a metadata object that tells Angular how to compile and launch the application.
 */
-@NgModule({ 
+@NgModule({
   declarations: [ // this application's lone component
     AppComponent,
     HeroesComponent,
@@ -26,7 +30,9 @@ the @NgModule decorator identifies AppModule as an NgModule class.
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    InMemoryDataService
   ],
   providers: [HeroService, MessageService], // the service providers
   bootstrap: [AppComponent] // the root component that Angular creates and inserts into the index.html host web page.
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-
+import {HttpClient, HttpHeaders} from '@angular/common/http'
 import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
 
@@ -9,19 +9,34 @@ import { HEROES } from './mock-heroes';
 
 @Injectable()
 export class HeroService {
-  constructor(private messageService: MessageService) { }
+  private heroesUrl = 'api/heroes'; // URL to web api
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService
+    ) {}
 
   /*
     send the message _after_ fetching the heroes
     of(HEROES) returns an Observable<Hero[]> that emits a single value, the array of mock heroes
   */
+  /*
   getHeroes(): Observable<Hero[]> {
     this.messageService.add('HeroService: fetched heroes');
     return of(HEROES);
+  }
+  */
+
+/** GET heroes from the server */
+  getHeroes(): Observable<Hero[]> {
+    return this.http.get<Hero[]>(this.heroesUrl);
   }
 
   getHero(id: number): Observable<Hero> {
     this.messageService.add(`HeroService: fetched hero id=${id}`);
     return of(HEROES.find(hero => hero.id === id));
+  }
+
+  private log(message: string){
+    this.messageService.add('HeroService: ' + message);
   }
 }
